d239cd5e7fd0fb7b930112505ee4ab78caab2e01
logan <767405394@qq.com>
2023-07-02 21:46:14 +0800

    完成sacss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,14 @@
 
 <template>
   一级路由出口组件
+  <div class="test">
+    test scss
+  </div>
   <router-view></router-view>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.test{
+  color: $priceColor;
+}
 </style>
