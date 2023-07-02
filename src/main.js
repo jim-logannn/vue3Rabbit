0da0dff63670c2039671a2d9e1fa05a154618f91
logan <767405394@qq.com>
2023-07-02 13:06:37 +0800

    axios的封装配置

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import {getCategory} from "@/aips/testApi.js"
 import App from './App.vue'
 import router from './router'
-
+getCategory().then(res=>console.log(res))
 const app = createApp(App)
 
 app.use(createPinia())
