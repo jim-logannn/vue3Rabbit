b4878d7fbc286ad767747abe8881c76c75774fed
logan <767405394@qq.com>
2023-07-02 12:58:03 +0800

    sass elementelementPlus主题色配置

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,25 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        //1、配置elementPlus采用sass配色系统
+        ElementPlusResolver({importStyle:"sass"})
+      ],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
