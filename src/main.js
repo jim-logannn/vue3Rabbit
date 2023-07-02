623162e54fa3a3220f03a7333474f7e6b7491094
logan <767405394@qq.com>
2023-07-02 21:31:39 +0800

    静态资源的创建

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia'
 import {getCategory} from "@/aips/testApi.js"
 import App from './App.vue'
 import router from './router'
+//引入初始化样式文件
+import "@/styles/common.scss"
 getCategory().then(res=>console.log(res))
 const app = createApp(App)
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>首页</div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/category">Category</router-link>
+    <router-view></router-view>
+</template>
