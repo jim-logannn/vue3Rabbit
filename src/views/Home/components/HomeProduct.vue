610f962b51344095e99ddf1514d58f0766991d98
logan <767405394@qq.com>
2023-07-08 22:48:06 +0800

    home-goodsItem组件封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HomePanel from './HomePanel.vue'
+import GoodItem from "./GoodItem.vue"
 import {getProductApi} from "@/apis/home.js"
 import { onMounted, ref } from 'vue'
 const goodList=ref([])
@@ -26,13 +27,8 @@ onMounted(()=>{
           </strong>
         </RouterLink>
         <ul class="goods-list">
-          <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="cate.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+          <li v-for="goods in cate.goods" :key="goods.id">
+            <GoodItem :goods="goods"></GoodItem>
           </li>
         </ul>
       </div>
@@ -127,41 +123,6 @@ onMounted(()=>{
       }
     }
 
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
-      }
-    }
   }
 }
 </style>
