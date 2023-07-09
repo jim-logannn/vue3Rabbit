a95f991520a504f627bdd2da13dbf126d4758ec8
logan <767405394@qq.com>
2023-07-09 19:32:35 +0800

    二级分类整体认识

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from "@/views/Login/index.vue"
 import Layout from "@/views/Layout/index.vue"
 import Home from "@/views/Home/index.vue"
 import Category from "@/views/Category/index.vue"
+import SubCategory from "@/views/SubCategory/index.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,11 @@ const router = createRouter({
         {
           path:"category/:id",
           component:Category
-        }
+        },
+        {
+          path:"category/sub/:id",
+          component:SubCategory
+        },
       ]
     },
     {
