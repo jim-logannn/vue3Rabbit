433747d7ddb66e5e917f64ffb54bedf44874bdb4
logan <767405394@qq.com>
2023-07-09 21:48:12 +0800

    二级分类-列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -28,6 +28,24 @@ const getGoodList=async ()=>{
 onMounted(()=>{
     getGoodList()
 })
+
+//tab切换回调
+const tabChange=()=>{
+    console.log(reqData.value.sortField);
+    reqData.value.page=1
+    getGoodList()
+}
+
+//底部加载更多
+const disabled=ref(false)
+const load=async ()=>{
+    reqData.value.page++
+    const {result:res}=await getSubCategoryAPI(reqData.value)
+    goodList.value=[...goodList.value,...res.items]
+    if(res.items.length==0){
+        disabled.value=true
+    }
+}
 </script>
 
 <template>
@@ -41,12 +59,12 @@ onMounted(()=>{
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
          <GoodItem v-for="item in goodList" :key="item.id" :goods="item"></GoodItem>
       </div>
