338d491adbd6facfa7f9fd3a622dbe4edfc8fc74
logan <767405394@qq.com>
2023-07-09 21:51:09 +0800

    二级分类-路由滚动行为体验优化

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,12 @@ const router = createRouter({
       path:"/login",
       component:Login
     }
-  ]
+  ],
+  scrollBehavior(){
+    return{
+      top:0
+    }
+  }
 })
 
 export default router
