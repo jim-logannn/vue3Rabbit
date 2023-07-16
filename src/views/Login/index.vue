493dd07bc926c5b93c73ac124f6f319710142308
logan <767405394@qq.com>
2023-07-16 16:25:10 +0800

    登录-整体认识和路由的配置

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,64 @@
+<script setup>
+
+</script>
+
+
 <template>
-    <div>登陆页面</div>
+  <div>
+    <header class="login-header">
+      <div class="container m-top-20">
+        <h1 class="logo">
+          <RouterLink to="/">小兔鲜</RouterLink>
+        </h1>
+        <RouterLink class="entry" to="/">
+          进入网站首页
+          <i class="iconfont icon-angle-right"></i>
+          <i class="iconfont icon-angle-right"></i>
+        </RouterLink>
+      </div>
+    </header>
+    <section class="login-section">
+      <div class="wrapper">
+        <nav>
+          <a href="javascript:;">账户登录</a>
+        </nav>
+        <div class="account-box">
+          <div class="form">
+            <el-form label-position="right" label-width="60px"
+              status-icon>
+              <el-form-item  label="账户">
+                <el-input/>
+              </el-form-item>
+              <el-form-item label="密码">
+                <el-input/>
+              </el-form-item>
+              <el-form-item label-width="22px">
+                <el-checkbox  size="large">
+                  我已同意隐私条款和服务条款
+                </el-checkbox>
+              </el-form-item>
+              <el-button size="large" class="subBtn">点击登录</el-button>
+            </el-form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="login-footer">
+      <div class="container">
+        <p>
+          <a href="javascript:;">关于我们</a>
+          <a href="javascript:;">帮助中心</a>
+          <a href="javascript:;">售后服务</a>
+          <a href="javascript:;">配送与验收</a>
+          <a href="javascript:;">商务合作</a>
+          <a href="javascript:;">搜索推荐</a>
+          <a href="javascript:;">友情链接</a>
+        </p>
+        <p>CopyRight &copy; 小兔鲜儿</p>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <style scoped lang='scss'>
