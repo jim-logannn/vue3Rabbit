19f562b3bbc8ae80180142aca54110ec74bf5c44
logan <767405394@qq.com>
2023-07-16 16:59:49 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -6,7 +6,8 @@ import { ref } from "vue";
 //准备表单对象
 const form=ref({
   account:"",
-  password:""
+  password:"",
+  agree:true
 })
 
 //准备规则对象
@@ -17,6 +18,17 @@ const rules={
   password:[
     {required:true,message:"密码不能为空",trigger:"blur"},
     {min:6,max:14,message:"密码长度需在6~14位",trigger:"blur"}
+  ],
+  agree:[
+    {
+      validator:(rule,value,callback)=>{
+        if(value){
+          callback()
+        }else{
+          callback(new Error("请勾选协议"))
+        }
+      }
+    }
   ]
 }
 </script>
@@ -45,14 +57,14 @@ const rules={
           <div class="form">
             <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
-              <el-form-item prop="account"  label="账户">
+              <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
               </el-form-item>
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox v-model="form.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
