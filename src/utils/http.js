a169ea9b7632995d476aa3219254211e2117348b
logan <767405394@qq.com>
2023-07-23 13:10:33 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
+import {useUserStore} from "@/stores/user"
 
 const httpInstance=axios.create({
     baseURL:"http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -11,6 +12,13 @@ const httpInstance=axios.create({
 //拦截器
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  // 1.从pinia获取token数据
+  const userStore=useUserStore()
+  const token=userStore.userInfo.token
+  // 2.按照后端的要求拼接token数据
+  if(token){
+    config.headers.Authorization=`Bearer ${token}`
+  }
     return config
   }, e => Promise.reject(e))
   
