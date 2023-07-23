dbe38c66a1e0d3480a7e31062513df91ba82920f
logan <767405394@qq.com>
2023-07-23 12:54:14 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 //引入初始化样式文件
@@ -10,9 +11,11 @@ import {lazyPlugin} from "@/directives/index"
 import { componentPlugin } from '@/components'
 
 const app = createApp(App)
-
+const pinia=createPinia()
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate)
 app.use(lazyPlugin)
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(componentPlugin)
 app.mount('#app')
