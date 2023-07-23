caa18fe2e7647c9bac6ecccb4c03fb38a8ddcdcc
logan <767405394@qq.com>
2023-07-23 13:30:43 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
 import {useUserStore} from "@/stores/user"
+import router from "@/router"
 
 const httpInstance=axios.create({
     baseURL:"http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -20,7 +21,7 @@ httpInstance.interceptors.request.use(config => {
     config.headers.Authorization=`Bearer ${token}`
   }
     return config
-  }, e => Promise.reject(e))
+}, e => Promise.reject(e))
   
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
@@ -28,6 +29,14 @@ httpInstance.interceptors.response.use(res => res.data, e => {
     type:"warning",
     message:e.response.data.message
   })
+  // 401 token失效处理
+  // 1.清除本地用户数据
+  // 2.跳转到登录页
+  if(e.response.status===401){
+    const userStore=useUserStore()
+    userStore.clearUserInfo()
+    router.push("/login")
+  }
   return Promise.reject(e)
 })
 export default httpInstance
