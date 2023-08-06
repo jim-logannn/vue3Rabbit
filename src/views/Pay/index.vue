bbee4272ed9fa74a3e824bb1aeb4bcc180201c9a
logan <767405394@qq.com>
2023-08-06 13:01:02 +0800

    支付功能实现

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -10,6 +10,13 @@ const getOrder=async()=>{
     payInfo.value=res.result
 }
 onMounted(()=>getOrder())
+
+//跳转支付
+//携带订单id以及回调地址跳转到支付地址（get）
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
 
 
