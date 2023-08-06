80fa9ac54abf6fb6b0ae4a330a223a44086be9da
logan <767405394@qq.com>
2023-08-06 20:31:22 +0800

    会员中心-功能理解以及路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,9 @@ import CartList from "@/views/CartList/index.vue"
 import Checkout from "@/views/Checkout/index.vue"
 import Pay from "@/views/Pay/index.vue"
 import PayBack from "@/views/Pay/PayBack.vue"
+import Member from "@/views/Member/index.vue"
+import UserInfo from "@/views/Member/components/UserInfo.vue"
+import UserOrder from "@/views/Member/components/UserOrder.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +53,20 @@ const router = createRouter({
         {
           path:"paycallback",
           component:PayBack
+        },
+        {
+          path:"member",
+          component:Member,
+          children:[
+            {
+              path:"user",
+              component:UserInfo
+            },
+            {
+              path:"order",
+              component:UserOrder
+            }
+          ]
         },
       ]
     },
