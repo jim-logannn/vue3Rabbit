66c0789fb937ef4336dce24ab32aab8c028676e4
logan <767405394@qq.com>
2023-08-06 12:54:21 +0800

    支付-获取基础支付信息

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,5 +1,15 @@
 <script setup>
-const payInfo = {}
+import {getOrderAPI} from "@/apis/pay.js"
+import { onMounted, ref } from "vue"
+import {useRoute} from "vue-router"
+//获取订单数据
+const route=useRoute()
+const payInfo = ref({})
+const getOrder=async()=>{
+    const res=await getOrderAPI(route.query.id)
+    payInfo.value=res.result
+}
+onMounted(()=>getOrder())
 </script>
 
 
